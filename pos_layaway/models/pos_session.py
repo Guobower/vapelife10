79d74625ef8afdee48fa19302b5a0e2f7f783092
Shivam Goyal <shivam1111@gmail.com>
2018-03-17 08:25:48 +0530

    removed a bug wherein the layaway orders are also not allowed to be closed

--- pos_layaway/models/pos_session.py
+++ pos_layaway/models/pos_session.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api,_
+from odoo.exceptions import UserError
 
 class PosSession(models.Model):
     _inherit = "pos.session"
@@ -19,8 +20,12 @@ class PosSession(models.Model):
             move = self.env['pos.order'].with_context(force_company=company_id)._create_account_move(session.start_at, session.name, int(journal_id), company_id)
             orders.with_context(force_company=company_id)._create_account_move_line(session, move)
             for order in session.order_ids.filtered(lambda o: o.state not in ['done', 'invoiced','layaway']):
-                if order.state not in ('paid'):
+                if order.state == 'paid':
+                    order.action_pos_order_done()
+                elif order.state == 'layaway':
+                    continue
+                else:
+                    print "=============order",order
                     raise UserError(_("You cannot confirm all orders of this session, because they have not the 'paid' status"))
-                order.action_pos_order_done()
             orders = session.order_ids.filtered(lambda order: order.state in ['invoiced', 'done'])
             orders.sudo()._reconcile_payments()        
